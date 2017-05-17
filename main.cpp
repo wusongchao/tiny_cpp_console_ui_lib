ed13e235c36622136fed28250066530a64c715ba
yutianhuihong <2256912385@qq.com>
2017-05-17 22:33:43 +0800

    add tinyxml2

--- main.cpp
+++ main.cpp
@@ -2,9 +2,18 @@
 #include"UIView.cpp"
 #include"UIHintViewController.h"
 #include"UIHintViewController.cpp"
+#include"tinyxml2.h"
+#include"tinyxml2.cpp"
+#include<iostream>
 
+using namespace std;
+using namespace tinyxml2;
 int main(void){
-    UIHintViewController *v1 = new UIHintViewController("Hello World!");
-    v1->loadView();
+//    UIHintViewController *v1 = new UIHintViewController("Hello World!");
+//    v1->loadView();
+    XMLDocument doc;
+    doc.LoadFile("test.xml");
+    const char* content = doc.FirstChildElement("Hello")->GetText();
+    cout << content;
     return 0;
 }
